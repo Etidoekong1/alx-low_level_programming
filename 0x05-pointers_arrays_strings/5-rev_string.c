962afb4e3d7b4ffe07f9e4cfee0ca002677a4418
Etido Ekong <etidoekong2001@gmail.com>
2023-09-18 23:09:17 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * rev_string _ Reverses a string
+ * rev_string - Reverses a string
  * @: The string to be modified
  * Return: void
  */
@@ -13,7 +13,7 @@ void rev_string(char *s)
 	char tmp;
 
 	while (s[index++])
-	len ++;
+	len++;
 
 	for (index = len - 1; index >= len / 2; index--)
 	{
