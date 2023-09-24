40c1228f501841c0b01dd4676cc9316a86e98c57
Etido Ekong <etidoekong2001@gmail.com>
2023-09-24 08:09:57 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,39 +1,28 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main _ Prints a Fizz Buzz program
- * Return: Always 0 (Success)
+ * main - prints "Fizz" for numbers multiplied by 3,
+ * prints "Buzz" for numbers multiplied by 5,
+ * prints "FizzBuzz" for numbers multiplied by 3 and 5
+ * for numbers 1-100
+ * Return: 0;
  */
-
 int main(void)
-
 {
-	int x;
+int i;
 
-	for (x = 1; x <= 100; x++)
-	{
-	if ((x % 3 == 0) && (x % 5 ==0))
-	{
-	printf("FizzBuzz");
-	}
-	else if (x % 3 == 0)
-	{
-	printf("Fizz");
-	}
-	else if (x % 5 == 0)
-	{
-	printf("Buzz");
-	}
-	else
-	{
-	printf("%d", x);
-	}
-	if (x != 100)
-	{
-	printf(" ");
-	}
-	}
-	printf("\n");
-	return (0);
+for (i = 1; i <= 99; i++)
+{
+if (i % 15 == 0)
+printf("FizzBuzz ");
+else if (i % 3 == 0)
+printf("Fizz ");
+else if (i % 5 == 0)
+printf("Buzz ");
+else
+printf("%i ", i);
+}
+printf("Buzz\n");
+return (0);
 }
